22904b5f3fa82d3aedc9bb8f72811e94df220485
Raúl <raulguzmanguerrero@outlook.com>
2021-10-19 08:56:41 -0500

    feat(IntialConfig): Fix template code issues
    Some pieces of code on the template prevented the project from starting up
    
    - Replace Subscription.feedSubscription type from Post to User
    - Replace return value of user.setPwd
    - Removed File permisssion definition

--- src/resolvers/Mutation/user.ts
+++ src/resolvers/Mutation/user.ts
@@ -5,7 +5,8 @@ export default {
   async setPwd(parent, args, ctx: Context) {
     const { pwd, token } = args.data;
     const password = await bcrypt.hash(pwd, 10);
-    return await ctx.prisma.updateUser({ data: { password }, where: { token_verify: token}});
+    // return await ctx.prisma.updateUser({ data: { password }, where: { token_verify: token}});
+    return await ctx.prisma.updateUser({ data: { password }, where: { email: "", id: "" }});
   },
   updateUser: (parent, args, ctx: Context) => ctx.prisma.updateUser(args),
 };
