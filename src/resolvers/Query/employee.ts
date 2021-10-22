af41ed035a669b4bd947cc3ff29be9e9b44060ee
Raúl <raulguzmanguerrero@outlook.com>
2021-10-22 18:30:53 -0500

    feat(Employees): Add employee and employees queries

--- src/resolvers/Query/employee.ts
+++ src/resolvers/Query/employee.ts
@@ -1,14 +1,11 @@
 import { Context } from "../../utils";
-import { EmployeeWhereUniqueInput } from "../../generated/prisma-client/index";
-import { hash } from "bcryptjs";
-import { ApolloError } from "apollo-server-errors";
-import { generateEmail } from "../../utils/email";
-import { CreateRecordOperations, UpdateRecordOperations } from "../index";
 
 export default {
     calculatePayroll: async(parent, args, ctx: Context) => {
         let {employee, DaysWorked} = args;
 
         return (await ctx.prisma.employee(employee)).salary * DaysWorked;
-    }
+    },
+    employee: async(parent, args, ctx: Context) => await ctx.prisma.employee(args.where),
+    employees: async(parent, args, ctx: Context) => await ctx.prisma.employees()
 };
