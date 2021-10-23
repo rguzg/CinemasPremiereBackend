0f57124b997b1b40b104e5a7eff26fd6a5d4bab7
Raúl <raulguzmanguerrero@outlook.com>
2021-10-23 05:44:41 -0500

    a

--- src/resolvers/Mutation/employee.ts
+++ src/resolvers/Mutation/employee.ts
@@ -2,7 +2,7 @@ import { Context } from "../../utils";
 import { EmployeeCreateInput, EmployeeUpdateInput, User, UserCreateInput } from "../../generated/prisma-client/index";
 import { hash } from "bcryptjs";
 import { ApolloError } from "apollo-server-errors";
-import { generateEmail } from "../../utils/email";
+import generateEmail from "../../utils/email";
 import { CreateRecordOperations, UpdateRecordOperations } from "../index";
 
 function GenerateBankAccount():string {
