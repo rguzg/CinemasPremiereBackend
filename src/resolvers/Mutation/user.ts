b85c26b147710ca9f82045a83270f1cf8ebe28d6
Raúl <raulguzmanguerrero@outlook.com>
2021-10-22 19:33:51 -0500

    feat(auth): Hash user password when creating using createUser mutation

--- src/resolvers/Mutation/user.ts
+++ src/resolvers/Mutation/user.ts
@@ -1,4 +1,5 @@
 import * as bcrypt from "bcryptjs";
+import { UserCreateInput } from "../../generated/prisma-client";
 import { Context } from "../../utils";
 
 export default {
@@ -7,6 +8,13 @@ export default {
     const password = await bcrypt.hash(pwd, 10);
     // return await ctx.prisma.updateUser({ data: { password }, where: { token_verify: token}});
     return await ctx.prisma.updateUser({ data: { password }, where: { email: "", id: "" }});
+  },
+  createUser: async (parent, args, ctx: Context) => {
+    let user: UserCreateInput = args.data;
+
+    user.password = await bcrypt.hash(user.password, 10);
+
+    return await ctx.prisma.createUser(args.data);
   },
   updateUser: (parent, args, ctx: Context) => ctx.prisma.updateUser(args),
 };
